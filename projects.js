c6e4c2cff00c6044a804016515d90e90b3017d10
W <111629334+wth-d@users.noreply.github.com>
2024-09-21 17:19:57 -0400

    some unused draft files

---
+++ projects.js
@@ -0,0 +1,47 @@
+// unused for now
+
+export default [
+  {
+      date: "2000",
+      image: "./assets/img/project-music-player.png",
+      title: "Music Player Web-App",
+      description: "A music streaming web app based on Django.",
+      tools: "Django, HTML, CSS, Bootstrap, SQLite, AWS S3, Heroku",
+      listItems: [
+      "Register/login to the web app(with OAuth-based Google Sign-In).",
+      "Search and filter songs based on language and singer.",
+      "Create multiple playlists and add/remove songs to/from playlist.",
+      "Scroll through recently played/viewed songs.",
+      ],
+      deployUrl: "https://galvanic-music.herokuapp.com/",
+      codeUrl: "https://github.com/varadbhogayata/music-player",
+  },
+  {
+      date: "2012",
+      image: "./assets/img/project-quizup-logo-1.png",
+      title: "Quiz Web-App",
+      description: "A quiz playing web app based on Django.",
+      tools: "Django, HTML, CSS, Bootstrap, SQLite, Heroku",
+      listItems: [
+      "Register/login to the web app(with OAuth-based Google Sign-In).",
+      "Play Quiz and see the leaderboard",
+      ],
+      deployUrl: "https://quiz-up-app.herokuapp.com/",
+      codeUrl: "https://github.com/varadbhogayata/QuizUp",
+  },
+  {
+      date: "2017",
+      image: "./assets/img/project-blog-logo.jpg",
+      title: "Blog Web-App",
+      description: "A simple and extensible blog web-app based on Flask.",
+      tools: "HTML, CSS, Bootstrap, Flask, SQLAlchemy, Postgresql, Python",
+      listItems: [
+      "Users can view posts and contact the admin via Contact Page.",
+      "Admin can Add, Delete, Update posts.",
+      ],
+      deployUrl: "https://flask-heroku-blog.herokuapp.com/",
+      codeUrl: "https://github.com/varadbhogayata/flask-blog",
+  }
+];
+
+
